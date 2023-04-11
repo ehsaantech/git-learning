0bf30ac4c10271e74998c14b548a6d44c38a066b
Haroon Khan <aitchkhan@gmail.com>
2023-04-11 16:25:20 +0500

    add sum method and invokation (#1)

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ console.log('hello world');
 console.log('capsule is not very smart');
 
 console.log('Shayan bhagne wala hai');
+
+
+function sum(a,b) {
+    return a+b
+}
+
+console.log(sum(1,2));
+console.log(sum(3,4));
